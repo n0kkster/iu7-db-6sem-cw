acbd984b9232112ef34f9e7221da7c974c30b90b
n0kkster <order.graph@gmail.com>
2026-02-18 16:34:24 +0300

    ComponentNode -> Component + Guid

--- Analyzer.Domain/Entities/Component.cs
+++ Analyzer.Domain/Entities/Component.cs
@@ -3,9 +3,9 @@ namespace Analyzer.Domain.Entities;
 using Analyzer.Domain.Enums;
 using Analyzer.Domain.Exceptions;
 
-public class ComponentNode
+public class Component
 {
-    public required int Id { get; init; }
+    public Guid Id { get; } = Guid.NewGuid();
     public required ComponentType Type { get; init; }
     public required string Name
     {
