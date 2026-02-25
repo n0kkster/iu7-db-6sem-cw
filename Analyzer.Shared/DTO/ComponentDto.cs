f35fc568c612695614b433a9ec777acf1abb2bad
n0kkster <order.graph@gmail.com>
2026-02-25 16:53:38 +0300

    докинул гуид в дто

--- Analyzer.Shared/DTO/ComponentDto.cs
+++ Analyzer.Shared/DTO/ComponentDto.cs
@@ -4,4 +4,4 @@ using Analyzer.Domain.Enums;
 using Analyzer.Domain.Entities;
 
 public record CreateComponentDto(ComponentType Type, string Name) { }
-public record ComponentDto(ComponentType Type, string Name, List<Link> Links) { }
+public record ComponentDto(Guid Id, ComponentType Type, string Name, List<Link> Links) { }
