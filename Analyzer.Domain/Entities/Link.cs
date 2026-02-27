6a5d10bf434aaca384dbbf925c0ecf77f5c78c1e
n0kkster <order.graph@gmail.com>
2026-02-27 21:42:21 +0300

    вынес линк из компонента

--- Analyzer.Domain/Entities/Link.cs
+++ Analyzer.Domain/Entities/Link.cs
@@ -4,6 +4,8 @@ using Analyzer.Domain.Enums;
 
 public class Link
 {
-    public Guid DependsOn { get; init; }
-    public LinkSeverity Severity { get; set; }
+    public required Guid SourceId { get; init; }
+    public required Guid TargetId { get; init; }
+    public required LinkSeverity Severity { get; set; }
+    public required ProtocolType Protocol { get; set; }
 }
