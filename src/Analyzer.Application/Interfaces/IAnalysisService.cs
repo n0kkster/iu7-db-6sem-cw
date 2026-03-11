7739bce80e328540521864b09e7554de8c8e5d83
n0kkster <order.graph@gmail.com>
2026-03-11 20:43:05 +0300

    chore: cleanup

--- src/Analyzer.Application/Interfaces/IAnalysisService.cs
+++ src/Analyzer.Application/Interfaces/IAnalysisService.cs
@@ -1,7 +1,5 @@
 namespace Analyzer.Application.Interfaces;
 
-using Analyzer.Shared.DTO;
-
 public interface IAnalysisService
 {
     Task<List<Guid>> GetImpactedComponentsAsync(Guid failedComponentId);
