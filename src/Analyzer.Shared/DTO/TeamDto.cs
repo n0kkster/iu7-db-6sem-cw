d574f45f10d57cd9da54c206042a84a3fc0c0f14
n0kkster <order.graph@gmail.com>
2026-03-31 19:33:04 +0300

    chore: dto на передачу команды

--- src/Analyzer.Shared/DTO/TeamDto.cs
+++ src/Analyzer.Shared/DTO/TeamDto.cs
@@ -5,3 +5,11 @@ public class CreateTeamDto
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 }
+
+public class TeamDto
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public IReadOnlyCollection<Guid> Members { get; init; } = [];
+}
