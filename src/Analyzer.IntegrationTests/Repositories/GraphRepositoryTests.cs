d4386b6853d51a75eaf560eea8b7e3cf57f64d37
n0kkster <order.graph@gmail.com>
2026-04-05 13:23:36 +0300

    feat: дополнил тесты для методов анализа

--- src/Analyzer.IntegrationTests/Repositories/GraphRepositoryTests.cs
+++ src/Analyzer.IntegrationTests/Repositories/GraphRepositoryTests.cs
@@ -45,20 +45,20 @@ public class GraphRepositoryTests(SharedNeo4jFixture fixture)
         var repository = new Neo4jGraphRepository(driver);
         var systemId = Guid.NewGuid();
 
-        var source = new Component 
-        { 
-            SystemId = systemId, 
-            Name = "Frontend", 
-            Description = "UI", 
-            Type = ComponentType.Unknown 
+        var source = new Component
+        {
+            SystemId = systemId,
+            Name = "Frontend",
+            Description = "UI",
+            Type = ComponentType.Unknown
         };
-        
-        var target = new Component 
-        { 
-            SystemId = systemId, 
-            Name = "Backend API", 
-            Description = "API", 
-            Type = ComponentType.Microservice 
+
+        var target = new Component
+        {
+            SystemId = systemId,
+            Name = "Backend API",
+            Description = "API",
+            Type = ComponentType.Microservice
         };
 
         await repository.AddComponentAsync(source);
@@ -96,32 +96,33 @@ public class GraphRepositoryTests(SharedNeo4jFixture fixture)
 
         // Целевая система
         await repository.AddComponentAsync(
-            new Component 
-            { 
-                SystemId = targetSystemId, 
-                Name = "C1", 
-                Description = "D1", 
-                Type = ComponentType.Database 
+            new Component
+            {
+                SystemId = targetSystemId,
+                Name = "C1",
+                Description = "D1",
+                Type = ComponentType.Database
             }
         );
 
         await repository.AddComponentAsync(
-            new Component 
-            { 
-                SystemId = targetSystemId, 
-                Name = "C2", Description = "D2", 
-                Type = ComponentType.Database 
+            new Component
+            {
+                SystemId = targetSystemId,
+                Name = "C2",
+                Description = "D2",
+                Type = ComponentType.Database
             }
         );
 
         // Другая система
         await repository.AddComponentAsync(
-            new Component 
-            { 
-                SystemId = otherSystemId, 
-                Name = "Noise", 
-                Description = "Noise", 
-                Type = ComponentType.Unknown 
+            new Component
+            {
+                SystemId = otherSystemId,
+                Name = "Noise",
+                Description = "Noise",
+                Type = ComponentType.Unknown
             }
         );
 
@@ -141,6 +142,71 @@ public class GraphRepositoryTests(SharedNeo4jFixture fixture)
         var repository = new Neo4jGraphRepository(driver);
         var systemId = Guid.NewGuid();
 
+        var compA = new Component
+        {
+            SystemId = systemId,
+            Name = "Service A",
+            Description = "A",
+            Type = ComponentType.Microservice
+        };
+
+        var compB = new Component
+        {
+            SystemId = systemId,
+            Name = "Service B",
+            Description = "B",
+            Type = ComponentType.Microservice
+        };
+
+        var compC = new Component
+        {
+            SystemId = systemId,
+            Name = "Database C",
+            Description = "C",
+            Type = ComponentType.Database
+        };
+
+        await repository.AddComponentAsync(compA);
+        await repository.AddComponentAsync(compB);
+        await repository.AddComponentAsync(compC);
+
+        await repository.AddLinkAsync(
+            new Link
+            {
+                SourceId = compA.Id,
+                TargetId = compB.Id,
+                Protocol = ProtocolType.REST,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link
+            {
+                SourceId = compB.Id,
+                TargetId = compC.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        // Act
+        var impactedIds = await repository.GetCascadingFailureImpactAsync(compC.Id);
+
+        // Assert
+        // compA и compB должны пострадать
+        impactedIds.Should().Contain(compB.Id);
+    }
+
+
+    [Fact]
+    public async Task GetCyclicDependenciesAsync_ShouldReturnCycles()
+    {
+        // Arrange
+        var driver = fixture.CreateDriver();
+        var repository = new Neo4jGraphRepository(driver);
+        var systemId = Guid.NewGuid();
+
         var compA = new Component 
         { 
             SystemId = systemId, 
@@ -154,14 +220,14 @@ public class GraphRepositoryTests(SharedNeo4jFixture fixture)
             SystemId = systemId, 
             Name = "Service B", 
             Description = "B", 
-            Type = ComponentType.Microservice 
+            Type = ComponentType.Microservice     
         };
-        
+
         var compC = new Component 
         { 
             SystemId = systemId, 
-            Name = "Database C",
-            Description = "C",
+            Name = "Database C", 
+            Description = "C", 
             Type = ComponentType.Database 
         };
 
@@ -173,27 +239,295 @@ public class GraphRepositoryTests(SharedNeo4jFixture fixture)
             new Link 
             { 
                 SourceId = compA.Id, 
-                TargetId = compB.Id, 
-                Protocol = ProtocolType.REST, 
-                Severity = LinkSeverity.High 
+                TargetId = compB.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
             }
         );
-    
+
         await repository.AddLinkAsync(
             new Link 
             { 
                 SourceId = compB.Id, 
-                TargetId = compC.Id, 
-                Protocol = ProtocolType.TCP, 
-                Severity = LinkSeverity.High 
+                TargetId = compC.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+        
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compC.Id, 
+                TargetId = compA.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
             }
         );
 
         // Act
-        var impactedIds = await repository.GetCascadingFailureImpactAsync(compC.Id);
+        var cycles = await repository.GetCyclicDependenciesAsync(systemId);
 
         // Assert
-        // compA и compB должны пострадать
+        cycles.Should().NotBeEmpty();
+        cycles.Should().HaveCount(1);
+
+        var cycleNodes = cycles.First();
+        cycleNodes.Should().Contain(compA.Id);
+        cycleNodes.Should().Contain(compB.Id);
+        cycleNodes.Should().Contain(compC.Id);
+    }
+
+    [Fact]
+    public async Task GetSinglePointsOfFailureAsync_ShouldReturnHighlyDependedNodes()
+    {
+        // Arrange
+        var driver = fixture.CreateDriver();
+        var repository = new Neo4jGraphRepository(driver);
+        var systemId = Guid.NewGuid();
+
+        var compA = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Serice A", 
+            Description = "A", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compB = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service B", 
+            Description = "B", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compC = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service C", 
+            Description = "C", 
+            Type = ComponentType.Microservice 
+        };
+
+        var authDb = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "AuthDB", 
+            Description = "Authentication database", 
+            Type = ComponentType.Database 
+        };
+
+        var coreDb = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "CoreDB", 
+            Description = "Core database", 
+            Type = ComponentType.Database 
+        };
+
+        foreach (var c in new[] { compA, compB, compC, authDb, coreDb })
+            await repository.AddComponentAsync(c);
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compA.Id, 
+                TargetId = authDb.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compB.Id, 
+                TargetId = authDb.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compC.Id, 
+                TargetId = authDb.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = authDb.Id, 
+                TargetId = coreDb.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+
+        var spofs = await repository.GetSinglePointsOfFailureAsync(systemId, threshold: 3);
+
+        // Assert
+        spofs.Should().ContainKey(authDb.Id)
+             .WhoseValue.Should().Be(3);
+
+        spofs.Should().ContainKey(coreDb.Id)
+             .WhoseValue.Should().Be(4);
+
+        spofs.Should().NotContainKey(compA.Id);
+    }
+
+    [Fact]
+    public async Task GetDecommissioningImpactAsync_ShouldReturnTransitiveDependents()
+    {
+        // Arrange
+        var driver = fixture.CreateDriver();
+        var repository = new Neo4jGraphRepository(driver);
+        var systemId = Guid.NewGuid();
+
+        var compA = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service A", 
+            Description = "A", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compB = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service B", 
+            Description = "B", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compC = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Database C", 
+            Description = "C", 
+            Type = ComponentType.Database 
+        };
+
+        var compD = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service D", 
+            Description= "D", 
+            Type = ComponentType.Microservice 
+        };
+
+
+        foreach (var c in new[] { compA, compB, compC, compD })
+            await repository.AddComponentAsync(c);
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compA.Id, 
+                TargetId = compB.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compB.Id, 
+                TargetId = compC.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compD.Id, 
+                TargetId = compC.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+
+        // Act
+        var impactedIds = await repository.GetDecommissioningImpactAsync(compC.Id);
+
+        // Assert
+        impactedIds.Should().HaveCount(3);
+        impactedIds.Should().Contain(compA.Id);
         impactedIds.Should().Contain(compB.Id);
+        impactedIds.Should().Contain(compD.Id);
+    }
+
+    [Fact]
+    public async Task GetDeploymentRiskPathsAsync_ShouldReturnAllPathsToTarget()
+    {
+        // Arrange
+        var driver = fixture.CreateDriver();
+        var repository = new Neo4jGraphRepository(driver);
+        var systemId = Guid.NewGuid();
+
+        var compA = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service A", 
+            Description = "A", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compB = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service B", 
+            Description = "B", 
+            Type = ComponentType.Microservice 
+        };
+
+        var compC = new Component 
+        { 
+            SystemId = systemId, 
+            Name = "Service C", 
+            Description = "C", 
+            Type = ComponentType.Database 
+        };
+
+        foreach (var c in new[] { compA, compB, compC })
+            await repository.AddComponentAsync(c);
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compA.Id, 
+                TargetId = compB.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        await repository.AddLinkAsync(
+            new Link 
+            { 
+                SourceId = compB.Id, 
+                TargetId = compC.Id,
+                Protocol = ProtocolType.TCP,
+                Severity = LinkSeverity.High
+            }
+        );
+
+        // Act
+        var riskPaths = await repository.GetDeploymentRiskPathsAsync(compC.Id);
+
+        // Assert
+        riskPaths.Should().HaveCount(2);
+        riskPaths.Should().Contain(p => p.NodeIds.SequenceEqual(new[] { compA.Id, compB.Id, compC.Id }));        
+        riskPaths.Should().Contain(p => p.NodeIds.SequenceEqual(new[] {compB.Id, compC.Id }));
     }
 }
